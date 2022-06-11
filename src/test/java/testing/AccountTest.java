ce9e2ebd0ae58c645f7fbfd13617612e9147806b
karol <karol.malysa@interia.pl>
2022-06-11 22:43:18 +0200

    lesson 4: test structure, given-when-then, name convention

--- src/test/java/testing/AccountTest.java
+++ src/test/java/testing/AccountTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * this test is bad, becouse more than one case is tested.
+ * its still ok, to know antipatterns, to not doing it
+ */
 public class AccountTest {
 
     /**
@@ -12,19 +16,25 @@ public class AccountTest {
      * second param is a message, that can be left as a suggestion for future treating this method
      */
     @Test
-    void myTest() {
-
+    void newAccountShouldNotBeActiveAfterCreation() {
+        //given (depend on team preferences can be "given + when" instead of one above another)
+        //when
         Acount newAccount = new Acount();
+
+        //then
         assertFalse(newAccount.isActive(), "check if new account is active" );
 
     }
 
     @Test
-    void myTest2() {
-
+    void accountShouldtBeActiveAfterActivation() {
+        //given
         Acount newAccount = new Acount();
-        assertFalse(newAccount.isActive());
+
+        //when
         newAccount.setActive(true);
+
+        //then
         assertTrue(newAccount.isActive());
     }
 }
